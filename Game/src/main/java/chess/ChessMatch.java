d081d768b55861f25afad7ba96dff1c6f4f470c7
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-21 15:00:42 -0300

    Moving pieces

--- Game/src/main/java/chess/ChessMatch.java
+++ Game/src/main/java/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -22,11 +24,35 @@ public class ChessMatch {
         return mat;
     }
 
+    public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+        Position source = sourcePosition.toPosition();
+        Position target = targetPosition.toPosition();
+
+        validadeSourcePosition(source);
+        Piece capturedPiece = makeMove(source, target);
+
+        return (ChessPiece)capturedPiece;
+    }
+
+    private Piece makeMove(Position source, Position target) {
+        Piece p = this.board.removePiece(source);
+        Piece capturedPiece = this.board.removePiece(target);
+        this.board.placePiece(p, target);
+        return capturedPiece;
+    }
+
+    private void validadeSourcePosition(Position position) {
+        if (!this.board.thereIsAPiece(position)) {
+            throw new ChessException("There is no piece on source position");
+        }
+    }
+
     private void placeNewPiece(char column, int row, ChessPiece piece) {
         this.board.placePiece(piece, new ChessPosition(column, row).toPosition());
     }
 
     private void initialSetup() {
+        placeNewPiece('c', 1, new Rook(board, Color.WHITE));
         placeNewPiece('c', 2, new Rook(board, Color.WHITE));
         placeNewPiece('d', 2, new Rook(board, Color.WHITE));
         placeNewPiece('e', 2, new Rook(board, Color.WHITE));
