f4d89fba4e366a374fe3c4dd2c94d995b10691a8
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-21 15:59:23 -0300

    Printing possible moves

--- Game/src/main/java/chess/ChessMatch.java
+++ Game/src/main/java/chess/ChessMatch.java
@@ -24,6 +24,12 @@ public class ChessMatch {
         return mat;
     }
 
+    public boolean[][] possibleMoves (ChessPosition sourcePostion) {
+        Position position = sourcePostion.toPosition();
+        validadeSourcePosition(position);
+        return this.board.piece(position).possibleMoves();
+    }
+
     public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
