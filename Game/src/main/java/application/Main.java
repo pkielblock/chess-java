e27740b6ca19f5c064cfc4c9f8597b98e13571d8
Pedro Kielblock <p.kielblock123@gmail.com>
2023-01-22 12:31:21 -0300

    promotion

--- Game/src/main/java/application/Main.java
+++ Game/src/main/java/application/Main.java
@@ -5,6 +5,7 @@ import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -34,6 +35,12 @@ public class Main {
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
                 if (capturedPiece != null) {
                     captured.add(capturedPiece);
+                }
+
+                if (chessMatch.getPromoted() != null) {
+                    System.out.print("Enter pice for promotion (B/N/R/Q): ");
+                    String type = sc.nextLine();
+                    chessMatch.replacePromotedPiece(type);
                 }
             } catch (ChessException | InputMismatchException e) {
                 System.out.println(e.getMessage());
